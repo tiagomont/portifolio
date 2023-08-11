22c09af34566194aa8e1891f64851277b88b3cc3
Tiago Monteiro Alves <tiagovga456@gmail.com>
2023-08-10 22:08:01 -0300

    starting projects part

---
+++ script.js
@@ -0,0 +1,14 @@
+function show_html()
+{
+	alert('HyperText Markup Language, é uma linguagem de marcação usada para estruturar o conteúdo de páginas web por meio de elementos como tags, que definem cabeçalhos, parágrafos, listas e incorporam mídia, criando a base semântica da página.')
+}
+
+function show_css()
+{
+	alert('Cascading Style Sheets, são folhas de estilo em cascata, uma linguagem que estiliza elementos HTML, controlando sua apresentação visual, posicionamento e layout. Usa seletores para escolher elementos e propriedades para aplicar estilos, separando o design do conteúdo.')
+}
+
+function show_js()
+{
+	alert('JavaScript, é umainguagem de programação interpretada pelo navegador, permite manipular dinamicamente elementos HTML, responder a eventos do usuário e acessar APIs para criar funcionalidades interativas, validação de formulários, requisições assíncronas e modificações em tempo real nas páginas.')
+}
