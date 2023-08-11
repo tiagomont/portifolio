60bd10c173b35abb7b19d4d339fc1b9272b6bf1a
Tiago Monteiro Alves <tiagovga456@gmail.com>
2023-08-11 01:34:29 -0300

    added git on my skills

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
 function show_html()
 {
-	alert('HyperText Markup Language, é uma linguagem de marcação usada para estruturar o conteúdo de páginas web por meio de elementos como tags, que definem cabeçalhos, parágrafos, listas e incorporam mídia, criando a base semântica da página.')
+	alert('HyperText Markup Language é uma linguagem de marcação usada para estruturar o conteúdo de páginas web por meio de elementos como tags, que definem cabeçalhos, parágrafos, listas e incorporam mídia, criando a base semântica da página.')
 }
 
 function show_css()
 {
-	alert('Cascading Style Sheets, são folhas de estilo em cascata, uma linguagem que estiliza elementos HTML, controlando sua apresentação visual, posicionamento e layout. Usa seletores para escolher elementos e propriedades para aplicar estilos, separando o design do conteúdo.')
+	alert('Cascading Style Sheets são folhas de estilo em cascata, uma linguagem que estiliza elementos HTML, controlando sua apresentação visual, posicionamento e layout. Usa seletores para escolher elementos e propriedades para aplicar estilos, separando o design do conteúdo.')
 }
 
 function show_js()
 {
-	alert('JavaScript, é umainguagem de programação interpretada pelo navegador, permite manipular dinamicamente elementos HTML, responder a eventos do usuário e acessar APIs para criar funcionalidades interativas, validação de formulários, requisições assíncronas e modificações em tempo real nas páginas.')
+	alert('JavaScript é uma linguagem de programação interpretada pelo navegador, permite manipular dinamicamente elementos HTML, responder a eventos do usuário e acessar APIs para criar funcionalidades interativas, validação de formulários, requisições assíncronas e modificações em tempo real nas páginas.')
+}
+
+function show_git()
+{
+	alert('Git é um sistema de controle de versão distribuído amplamente utilizado para o rastreamento de mudanças em projetos de software. Ele permite que várias pessoas colaborem em um projeto, mantendo um histórico detalhado das edições feitas em arquivos. Com recursos como ramificações (branches) e mesclagens (merges), o Git facilita o desenvolvimento paralelo, experimentação e colaboração eficiente, ao mesmo tempo em que preserva a integridade do código-fonte.')
 }
