83688becc7d23640cdcacafded05a51e94fc7ffa
Tiago Monteiro Alves <tiagovga456@gmail.com>
2024-10-11 02:12:47 -0300

    form finalized

--- scripts/form.js
+++ scripts/form.js
@@ -2,22 +2,27 @@ document.getElementById('contact_form').addEventListener('submit', function(even
     event.preventDefault(); // Evita o comportamento padrão de envio do formulário
   
     // Pega os valores dos campos
-    const name = document.getElementById('name').value;
-    const email = document.getElementById('email').value;
+    const nome = document.getElementById('from_name').value;
+    const e_mail = document.getElementById('from_email').value;
     const proposal = document.getElementById('proposal').value;
   
     // Configura o EmailJS
-    emailjs.init('y1fXVUYtUZxYt97Hm'); // Substitua pelo seu User ID do EmailJS
+    emailjs.init('GKiUff51LXSvcbmsL'); // Substitua pelo seu User ID do EmailJS
   
     const templateParams = {
-      to_name: name,
-      from_email: email,
-      message_html: `Olá, sou ${name}, meu email é ${email} e a minha proposta: ${proposal}`
+      form_name: nome,
+      form_email: e_mail,
+      proposal: proposal
     };
   
-    emailjs.send('service_djmy37s', 'template_xzasxpt', templateParams)
+    emailjs.send('service_djmy37s', 'template_tjq3sge', templateParams)
       .then(function(response) {
         console.log('Email enviado com sucesso!', response.status, response.text);
+        // Avisa o usuário que o formulário foi enviado
+        alert("Proposta enviada!")
+
+        // Limpa os campos do formulário após o envio bem-sucedido
+        document.getElementById('contact_form').reset();
       }, function(error) {
         console.log('Falha ao enviar email...', error);
       });
